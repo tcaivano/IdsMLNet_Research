49bfd93309ba6be44aa5595511af5e09bba2c755
Tony Caivano <italiancaivano@gmail.com>
2023-05-24 21:31:02 -0400

    Add nearest neighbor calculation

--- IdsMLNet_Research/Data/AuthEventTransform.cs
+++ IdsMLNet_Research/Data/AuthEventTransform.cs
@@ -1,6 +1,10 @@
-﻿using CsvHelper.Configuration.Attributes;
+﻿using CsvHelper.Configuration;
+using CsvHelper;
+using CsvHelper.Configuration.Attributes;
+using Microsoft.ML.Data;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,22 +14,31 @@ namespace IdsMLNet_Research.Data
     public class AuthEventTransform
     {
         [Index(0)]
+        [LoadColumn(0)]
         public int TimeStamp { get; set; }
         [Index(1)]
+        [LoadColumn(1)]
         public string? SourceUser { get; set; }
         [Index(2)]
+        [LoadColumn(2)]
         public string? DestinationUser { get; set; }
         [Index(3)]
+        [LoadColumn(3)]
         public string? SourceComputer { get; set; }
         [Index(4)]
+        [LoadColumn(4)]
         public string? DestinationComputer { get; set; }
         [Index(5)]
+        [LoadColumn(5)]
         public string? LogonType { get; set; }
         [Index(6)]
+        [LoadColumn(6)]
         public bool AuthenticationOrientation { get; set; }
         [Index(7)]
+        [LoadColumn(7)]
         public bool IsSuccessful { get; set; }
         [Index(8)]
+        [LoadColumn(8)]
         public bool IsRedTeam { get; set; }
 
         /// <summary>
@@ -59,14 +72,63 @@ namespace IdsMLNet_Research.Data
             // Is Successful Comparison
             x7 = Convert.ToInt16(a1.IsSuccessful != a2.IsSuccessful);
 
-            // Is Successful Comparison
+            // Is Red Team Comparison
             x8 = Convert.ToInt16(a1.IsRedTeam != a2.IsRedTeam);
 
             return Math.Sqrt(Math.Pow(x1, 2) + Math.Pow(x2, 2) + Math.Pow(x3, 2) + Math.Pow(x4, 2) + Math.Pow(x5, 2) + Math.Pow(x6, 2) + Math.Pow(x7, 2) + Math.Pow(x8, 2));
         }
 
+
+        public static List<Tuple<AuthEventTransform, double>> GetNearestNeighbors(AuthEventTransform a, AuthEventTransform[] dataSet, int k)
+        {
+            List<Tuple<AuthEventTransform, double>> result = new List<Tuple<AuthEventTransform, double>>();
+
+            for (int i = 0; i < dataSet.Length; i++)
+            {
+                var distance = GetEulcideanDistance(a, dataSet[i]);
+                var tup = new Tuple<AuthEventTransform, double>(dataSet[i], distance);
+                result.Add(tup);
+            }
+
+            return result.OrderBy(x => x.Item2).Take(k).ToList();
+        }
+
+        public static List<Tuple<AuthEventTransform, double>> GetNearestNeighbors(AuthEventTransform a, string dataSetLocation, int k)
+        {
+            List<Tuple<AuthEventTransform, double>> result = new List<Tuple<AuthEventTransform, double>>();
+            for (int i = 0; i < k; i++)
+            {
+                result.Add(new Tuple<AuthEventTransform, double>(null, int.MaxValue));
+            }
+
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = false
+            };
+            var total = 0;
+            using (var readerTruth = new StreamReader(dataSetLocation))
+            using (var csvTruth = new CsvReader(readerTruth, config))
+            {
+                while (csvTruth.Read())
+                {
+                    total++;
+                    var record = csvTruth.GetRecord<AuthEventTransform>();
+                    if (record == null) continue;
+                    var distance = GetEulcideanDistance(a, record);
+                    Console.Write("\rtotal: {0}      ", total.ToString("N0"));
+                    if (distance >= result.Last().Item2) continue;
+                    var tup = new Tuple<AuthEventTransform, double>(record, distance);
+                    result.Add(tup);
+                    result = result.OrderBy(x => x.Item2).Take(k).ToList();
+                }
+            }
+
+            return result.OrderBy(x => x.Item2).Take(k).ToList();
+        }
+
         private static int GetTupleStringDistance(string a1, string a2)
         {
+            int result;
             var a1s = a1.Split('@');
             var a2s = a2.Split('@');
 
@@ -75,18 +137,19 @@ namespace IdsMLNet_Research.Data
             if (set1Match && set2Match)
             {
                 // Both sets match
-                return 0;
+                result = 0;
             }
             else if (set1Match || set2Match)
             {
                 // Only one set matches
-                return 1;
+                result = 1;
             }
             else
             {
                 // Neither set matches
-                return 2;
+                result = 2;
             }
+            return result;
         }
     }
 }
